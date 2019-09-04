a6bcb5723b1b5777622c1b8c9249a6f46134b762
Dherendra Dev <42056549+dev1031@users.noreply.github.com>
2019-09-04 12:47:03 +0530

    Error and loading option
    Added Error and loading when the data is not fatched by the application

--- App.js
+++ App.js
@@ -8,30 +8,43 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state= {
-      hits: []
+      hits: [],
+      isLoading : false,
+      error: null
     }
   }
   componentDidMount() {
+    this.setState({isLoading : true});
     fetch(API + DEFAULT_QUERY)
-      .then(response =>response.json())
-      .then(data => this.setState({ hits: data.hits }));
-  }
-  render(){
-    const {hits} = this.state;
-    return (
-      <div className = "app">
-        <h1>News from Hacker Hunt</h1>
-      <ul>
-        {hits.map(hit =>
-          <li key={hit.objectID}>
-            <a href={hit.url}>{hit.title}</a>
-          </li>
-        )}
-      </ul>
-      
-      </div>
-    );
-  }
+      .then(response =>{
+        if(response.ok){
+            return(response.json())
+        }else{ 
+            throw new Error('Something went wrong...')
+        }})
+      .then(data => this.setState({ hits: data.hits, isLoading : false}))
+      .catch(error=>this.setState({error, isLoading:false}));
+      }
+    render(){
+      const {hits,isLoading,error} = this.state;
+      if(error){
+        return(<h1>{error.message}</h1>)
+      }
+      if(isLoading){
+        return(<p className="loading">Loading...</p>)
+      }
+      return (
+          <div className = "app">
+            <h1>News from Hacker Hunt</h1>
+            <ul>
+                {hits.map(hit =>
+                     <li key={hit.objectID}>
+                          <a href={hit.url}>{hit.title}</a>
+                     </li>)}
+             </ul>
+          </div>
+                );
+              }
 }
 
 export default App;
